d3fbecc9bd901e398f21c00df559209a9758b6a7
Julia Baugh <juliatealbaugh@gmail.com>
2018-01-02 21:43:02 -0800

    Made a color picker site

---
+++ homework/rgb-color-choice/js/project.js
@@ -0,0 +1,44 @@
+// 1. Use document.querySelector() and assign the .onclick event to #color-button
+
+document.querySelector('#color-button').onclick = changeColor
+
+// 2. Write a function, named changeColor, that is called when #color-button is clicked
+
+
+// 3. Inside of the function changeColor, use variables to store the <input> values from #red, #green, and #blue
+// - Use .value to get the values from the inputs
+// HINT: var red = document.querySelector('#red').value;
+
+function changeColor (colorStr) {
+	var red = document.querySelector('#red').value;
+	var blue = document.querySelector('#blue').value;
+	var green = document.querySelector('#green').value;
+
+	colorStr = parseInt(colorStr);
+
+
+
+
+// 4. Create a variable, named colorStr, which concatenates the above red, green, and blue variables into the format:
+// rgb(x, y, z)
+// If the user enters: 100, 150, and 200, colorStr should be: rgb(100, 150, 200)
+
+var colorStr = "rgb(" + red + "," + green + "," + blue + ")";
+
+
+// 5. Use .innerHTML to change the text inside of #colorful-text to colorStr
+
+// document.querySelector('#colorful-text').innerHTML = "rgb" + "(" + red + "," + green + "," + blue +")";
+
+document.querySelector('#colorful-text').innerHTML = colorStr;
+
+// 6. Use .style.background to change the background of #wrapper to colorStr
+
+document.querySelector('#wrapper').style.background = colorStr;
+
+}
+
+
+
+
+           
