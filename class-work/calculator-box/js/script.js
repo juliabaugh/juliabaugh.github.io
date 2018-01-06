9e72c3b78421a51cce75f51c2dc258b62581d9c1
Julia Baugh <juliatealbaugh@gmail.com>
2018-01-05 20:32:27 -0800

    Made it work but needs some simplification

--- class-work/calculator-box/js/script.js
+++ class-work/calculator-box/js/script.js
@@ -2,29 +2,62 @@
 function blueBackground () {
 	$('#out').css('background-color', 'blue')
 }
-
 $('#blue').click(blueBackground);
-
-
 
 
 function redBackground () {
 	$('#out').css('background-color', 'red')
 }
-
 $('#red').click(redBackground);
 
 
-var currentValue = $('#out').html();
-var a10 = $('#a10').html();
-var a20 = $('#a20').html();
-var a30 = $('#a30').html();
-
-currentValue = parseInt(currentValue);
-
-function addValue (){
-	(a10 + currentValue)
+function addA10 (){
+	var currentValue = $('#out').html();
+	currentValue = parseInt(currentValue);
+	currentValue = currentValue + 10;
+	$('#out').html(currentValue);
 }
+$('#a10').click(addA10);
 
-$('#a10').click(addValue);
+function addA20 (){
+	var currentValue = $('#out').html();
+	currentValue = parseInt(currentValue);
+	currentValue = currentValue + 20;
+	$('#out').html(currentValue);
+}
+$('#a20').click(addA20);
+
+function addA30 (){
+	var currentValue = $('#out').html();
+	currentValue = parseInt(currentValue);
+	currentValue = currentValue + 30;
+	$('#out').html(currentValue);
+}
+$('#a30').click(addA30);
+
+function subtract10 (){
+	var currentValue = $('#out').html();
+	currentValue = parseInt(currentValue);
+	currentValue = currentValue - 10;
+	$('#out').html(currentValue);
+}
+$('#n10').click(subtract10);
+
+function subtract20 (){
+	var currentValue = $('#out').html();
+	currentValue = parseInt(currentValue);
+	currentValue = currentValue - 20;
+	$('#out').html(currentValue);
+}
+$('#n20').click(subtract20);
+
+function subtract30 (){
+	var currentValue = $('#out').html();
+	currentValue = parseInt(currentValue);
+	currentValue = currentValue - 30;
+	$('#out').html(currentValue);
+}
+$('#n30').click(subtract30);
+
+//I'm sure there is a more simple way to write this!!
 
