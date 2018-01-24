d8000a81931c4550c0f1a141a02db9ed8c338546
Julia Baugh <juliatealbaugh@gmail.com>
2018-01-23 21:13:52 -0800

    final project updates

--- final project/js/script.js
+++ final project/js/script.js
@@ -7,13 +7,16 @@ function show (){
 }
 
 function changeText (){
-	$('.about').html("hide");
 
-	/*var aboutText = $('.about').html();
+	//$('.about').html("hide");
+
+	var aboutText = $('.about').html();
 
 	if (aboutText === "hide"){
-	$('.about').html("about");
-	} else $('.about').html("hide");*/
+		$('.about').html("about");
+	} else {
+		$('.about').html("hide");		
+	}
 }
 
 
@@ -41,6 +44,23 @@ $(this).click(NextImage);
 
 // also I want to set up right and left buttons that you click on to select the next or previous photo
 
+  var stickyNavTop = $('.subpages').offset().top;
+
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > stickyNavTop) { 
+      $('.subpages').addClass('sticky');
+    } else {
+      $('.subpages').removeClass('sticky'); 
+    }
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+    stickyNav();
+  });
 
 
 
