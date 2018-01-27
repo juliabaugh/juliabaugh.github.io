519608b1f0fffc24fb1f2e48fc41e8bf2b24868c
Julia Baugh <juliatealbaugh@gmail.com>
2018-01-27 11:18:14 -0800

    classwork files

---
+++ class-work/raindrops-example/js/app.js
@@ -0,0 +1,12 @@
+jQuery('#example2').raindrops(
+{color:'#99d4a5',
+canvasHeight:200});
+
+jQuery('#example1').raindrops();
+
+jQuery('#example5').raindrops(
+{color:'#f77b7b',
+canvasHeight:150, 
+waveLength: 100,
+ rippleSpeed: 0.05, 
+density: 0.04});
